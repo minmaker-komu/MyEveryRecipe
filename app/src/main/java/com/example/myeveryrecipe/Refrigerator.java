d9ae4710666ac82627abadf2e92528f20dadc539
minyeong <ninja2319@naver.com>
2022-06-02 22:37:28 +0900

    액티비티 반 완성

---
+++ app/src/main/java/com/example/myeveryrecipe/Refrigerator.java
@@ -0,0 +1,14 @@
+package com.example.myeveryrecipe;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Refrigerator extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_refrigerator);
+    }
+}
---
+++ app/src/main/java/com/example/myeveryrecipe/Sandwich.java
@@ -0,0 +1,14 @@
+package com.example.myeveryrecipe;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Sandwich extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sandwich);
+    }
+}
